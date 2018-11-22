4c8e994a03113ea9b9522297980fd42aec58c160
ChaYeJin1 <cha9599831@daum.net>
2018-11-22 15:34:38 +0900

    12_03

--- 12_1.c
+++ 12_1.c
@@ -4,16 +4,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	char src[100]="programming course";
-	char dst[100];
-	int i=0;
+	FILE *fp;
+	char input[100];
+	int i;
 	
-	while(src[i] != '\0')
-	{
-		i++;
+	fp=fopen("sample.txt", "w");
+	
+    for(i=0; i<3; i++)
+    {
+    	printf("input a word : ");
+    	scanf("%s", input);
+    	fprintf(fp, "%s\n", input);
 	}
-	
-	printf("%s (%i)\n", src, i);
+	 
+	fclose(fp);
 	
 	return 0;
 }
